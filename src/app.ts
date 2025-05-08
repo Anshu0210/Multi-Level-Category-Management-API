3e2c474817c3728fa0c2bef3784527fb42265af3
Anshu0210 <shahanshu0210@gmail.com>
2025-05-08 08:17:15 +0530

    code optimization

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Connect to database
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/category', categoryRoutes);
 
 // Start server
-const port = process.env.PORT || 8000;
 connectDB().then(() => {
+	const port = process.env.PORT || 8000;
 	app.listen(port, () => {
 		console.log(`Server running on port ${port}`);
 	});
